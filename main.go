9c0d921185fd54976d154a79dad113b652a715f2
Craig Warner <cgwarner2012@gmail.com>
2024-09-04 21:15:37 -0500

    feature: roam feature added

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 //	"github.com/hjson/hjson-go/v4"
     "encoding/json"
 
-	//	"math"
+	"math"
+	"math/rand"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
@@ -146,6 +147,31 @@ const (
 	]`
 
 )
+
+type tappableRaster struct {
+	fyne.CanvasObject
+	OnTapped func()
+}
+
+func NewTappableRaster(raster fyne.CanvasObject, onTapped func()) *tappableRaster{
+	return &tappableRaster{CanvasObject: raster, OnTapped: onTapped}
+}
+
+func (t *tappableRaster) Tapped(ev *fyne.PointEvent) { 
+	fmt.Println("x,y:",ev.Position.X,ev.Position.Y)
+	t.OnTapped() }
+
+//func DoRasterTap(ev *fyne.PointEvent) {
+func DoRasterTap() {
+	fmt.Println("Tapped")
+}
+
+
+//func (t *tappableRaster) pixelColor(x,y,w,h int) color.Color { 
+//	fmt.Println( "x,y",x,y,w,h)
+//	return(color.Black)
+//}
+
 // Field Names MUST start with a capital letter
 type MandelColor struct {
   	Ibits int 
@@ -167,7 +193,7 @@ type Mandel struct {
 	threshold float64 
 	span float64 
 	span_one_dot float64 
-	min_x,max_x,min_y,max_y float64 
+	min_x,min_y float64 
 	// Window
 	cur_w, cur_h int
 	// Colors
@@ -175,6 +201,20 @@ type Mandel struct {
 	all_color_names []string
 	cur_color_num int
 	new_color_num int
+	// Zoom Tap 
+	cur_zoom float64
+	new_zoom float64
+	// Roam 
+	cur_do_roam bool
+	new_do_roam bool
+	cur_roam_speed int // 1 to 100 (fast) 
+	new_roam_speed int 
+	cur_roam_total_steps int 
+	roam_tgt_x float64
+	roam_tgt_y float64
+	roam_tgt_span_adj float64 // 0.1-0.99
+	roam_zoom_in bool
+	roam_zoom_step int
 }
 
 type Color struct {
@@ -261,9 +301,18 @@ func (m *Mandel) AdvanceToNextDot() {
 func (m *Mandel) ResetBasic() {
 	// Reset Drawing 
 	m.up_to_date = false
-	m.cur_granularity = 16
+	m.cur_granularity = 64 
 	m.cur_x = 0
 	m.cur_y = 0
+}
+
+func (m *Mandel) ResetSpan() {
+	m.span= 3.0
+	m.min_x= -1.0
+//	m.max_x= 2.0
+	m.min_y= -1.5
+//	m.max_y= 1.5
+	m.span_one_dot = m.span / float64(m.size)
 }
 
 func (m *Mandel) ResetWindow(w, h int) {
@@ -282,9 +331,9 @@ func (m *Mandel) ResetWindow(w, h int) {
     } else {
 		max_val = h
     }
-	max_mult16 := (max_val / 16) * 16
+	max_mult64 := (max_val / 64) * 64 
 		// scale
-	m.size = max_mult16
+	m.size = max_mult64
 	m.span_one_dot = m.span / float64(m.size)
 	// Reset Drawing 
 	m.ResetBasic()
@@ -304,13 +353,13 @@ func (m *Mandel) DrawOneDot (px, py, w, h int) color.Color {
 	//fmt.Println("px:",px,"py:",py,"w:",w,"h:",h)
 	idx_x := 0
 	idx_y := 0
-	gran := 16
+	gran := 64 
 	if m.up_to_date {
 	    idx_x = use_px
 	    idx_y = use_py
 	} else {
-		if( m.cur_granularity == 16) {
-			gran = 16
+		if( m.cur_granularity == 64) {
+			gran = 64 
 		} else if( use_py < m.cur_y) {
 			gran = m.cur_granularity
 		} else {
@@ -333,11 +382,86 @@ func (m *Mandel) Status() {
 	fmt.Println(m.up_to_date,m.cur_granularity,m.cur_x,m.cur_y)
 }
 func (m *Mandel) UpdateSome() {
-	for bundle:=0;bundle<256;bundle++ {
+	bsize :=0
+	if (m.cur_granularity == 64) {
+		bsize = 4 
+	} else if (m.cur_granularity == 32) {
+		bsize = 16 
+	} else if (m.cur_granularity == 16) {
+		bsize = 64 
+	} else if (m.cur_granularity == 8) {
+		bsize = 256 
+	} else if (m.cur_granularity == 4) {
+		bsize = 1024 
+	} else if (m.cur_granularity == 2) {
+		bsize = 4096 
+	} else if (m.cur_granularity == 1) {
+		bsize = 4096  * 4
+	}
+	for bundle:=0;bundle<bsize;bundle++ {
 		m.CalcOneDot()
 		m.AdvanceToNextDot()
 	}
 //	m.Status()
+}
+
+func  RoamCalcDistance(x,y float64) float64 {
+	distance := math.Sqrt(x*x + y*y)
+	return(distance)
+}
+
+func (m *Mandel) RoamGenNewTgt() {
+	new_x := 0.0
+	new_y := 0.0
+	distance := 0.0
+	for distance < 1.5 {
+	    new_x = float64(rand.Intn(100)) / 100.0 * 3
+	    new_y = float64(rand.Intn(100)) / 100.0 * 3
+		distance = RoamCalcDistance(new_x,new_y)
+	}
+	m.roam_tgt_x = new_x -1.0
+	m.roam_tgt_y = new_y -1.5
+}
+
+
+func (m *Mandel) RoamAdjust() {
+//	fmt.Println("roam:",m.roam_zoom_step,"tx:",m.roam_tgt_x,m.min_x)
+	m.up_to_date = false
+	if(m.roam_zoom_in) {
+		if(m.roam_zoom_step == m.cur_roam_total_steps ) {
+			m.roam_zoom_in = false	
+			m.roam_zoom_step = 0	
+		} else {
+			real_crts  := float64(m.cur_roam_total_steps) 
+			delta_x := ((1.0 + m.roam_tgt_x)/ real_crts)
+			delta_y := ((1.5 + m.roam_tgt_y)/ real_crts)
+			m.min_x = m.min_x + delta_x
+			m.min_y = m.min_y + delta_y
+			// Reduce span
+			m.span = m.span * m.roam_tgt_span_adj
+			m.span_one_dot = m.span / float64(m.size)
+			// advance step 
+			m.roam_zoom_step++
+		}
+	} else {
+		if(m.roam_zoom_step == m.cur_roam_total_steps ) {
+			m.roam_zoom_in = true 
+			m.roam_zoom_step = 0	
+			m.ResetSpan()
+			m.RoamGenNewTgt()
+		} else {
+			real_crts  := float64(m.cur_roam_total_steps) 
+			delta_x := ((1.0 + m.roam_tgt_x)/ real_crts)
+			delta_y := ((1.5 + m.roam_tgt_y)/ real_crts)
+			m.min_x = m.min_x - delta_x
+			m.min_y = m.min_y - delta_y
+			// Increase span
+			m.span = m.span * (1.0 + (1.0-m.roam_tgt_span_adj))
+			m.span_one_dot = m.span / float64(m.size)
+			// advance step 
+			m.roam_zoom_step++
+		}
+	} 
 }
 
 func NewMandel() *Mandel {
@@ -346,22 +470,37 @@ func NewMandel() *Mandel {
 		size:256,
 		cur_x:0,
 		cur_y:0,
-		cur_granularity: 16,
+		cur_granularity: 64,
 		up_to_date: false,
 		// Math
 		span: 3.0,
 		threshold:100,
 		min_x: -1.0,
-		max_x: 2.0,
+//		max_x: 2.0,
 		min_y: -1.5,
-		max_y: 1.5,
+	//	max_y: 1.5,
 		//Window
 		cur_w: 256,
 		cur_h: 256,
 		// Color
 		cur_color_num: 0,
 		new_color_num: 0,
+		// Zoom
+		cur_zoom: 2.0,
+		new_zoom: 2.0,
+		// Roam
+		cur_do_roam: true,
+		new_do_roam: true,
+		cur_roam_speed: 5, // 100 is fast
+		new_roam_speed: 5, // 100
+		cur_roam_total_steps: 100,
+		roam_tgt_x: 1.5,
+		roam_tgt_y: 0.0, 
+		roam_tgt_span_adj: 0.98,
+		roam_zoom_in: true,
+		roam_zoom_step: 0,
 	}
+	m.RoamGenNewTgt()
 	m.span_one_dot = m.span / float64(m.size)
 	m.tiles = make([][]Color, max_size)       
     for i:=0;i<max_size;i++ {
@@ -384,7 +523,7 @@ func NewMandel() *Mandel {
         fmt.Printf("Unable to marshal JSON due to %s", err)
 		panic(1)
     }
-	fmt.Println(m.all_colors)
+	//fmt.Println(m.all_colors)
 	//fmt.Println("extra",lcl_all_colors[0].Ibits)
 	//fmt.Println("extra",lcl_all_colors[0].Blue_pos)
 	err = json.Unmarshal([]byte(all_color_names_str), &m.all_color_names)
@@ -392,7 +531,7 @@ func NewMandel() *Mandel {
         fmt.Printf("Unable to marshal JSON due to %s", err)
 		panic(1)
     }
-	fmt.Println(m.all_color_names)
+	//fmt.Println(m.all_color_names)
 	return m
 }
 
@@ -412,6 +551,15 @@ func (m *Mandel) ColorSettingsCallback(s string) {
 	}
 }
 
+func (m *Mandel) DoZoomChange(reset bool) {
+	m.cur_zoom = m.new_zoom
+	if(reset) {
+		m.ResetSpan()
+		m.ResetBasic()
+	}
+}
+
+
 func main() {
 	
 	myApp := app.New()
@@ -427,12 +575,12 @@ func main() {
 		fmt.Println("In Color Settings")
 		var popup *widget.PopUp
 
-		hello := widget.NewLabel("Color Hello")
+		color_hello := widget.NewLabel("Coloring Scheme")
 		colorSelect := widget.NewSelect(myMandel.all_color_names,myMandel.ColorSettingsCallback)
 		//okPopUpButton := widget.NewButton("Ok",ColorSettingsCallbackOk)
 		//cancelPopUpButton := widget.NewButton("Ok",ColorSettingsCallbackCancel)
 		popUpContent := container.NewVBox(
-			hello,
+			color_hello,
 			colorSelect,
 			container.NewHBox(
 				widget.NewButton("Ok",func() {
@@ -448,10 +596,45 @@ func main() {
 		popup = widget.NewModalPopUp(popUpContent,myWindow.Canvas())
 		popup.Show()
 	})
+	/*
+	menuItemZoom:= fyne.NewMenuItem("Zoom Settings", func() {
+		fmt.Println("In Zoome Settings")
+		var popup *widget.PopUp
+
+		zoom_hello := widget.NewLabel("Zoom Out / Zoom In")
+		zoom_slider:= widget.NewSlider(-10.0,10.0)
+		zoom_slider.SetValue(myMandel.cur_zoom)
+		zoom_slider.OnChanged = func(v float64) {
+            fmt.Println("Slider changed:", v)
+			myMandel.new_zoom = v 
+    	}
+		popUpContent := container.NewVBox(
+			zoom_hello,
+			zoom_slider,
+			container.NewHBox(
+				widget.NewButton("Ok",func() {
+					myMandel.DoZoomChange(false)
+					popup.Hide()
+				},),
+				widget.NewButton("Cancel",func() {
+					popup.Hide()
+				},),
+				widget.NewButton("Reset",func() {
+					myMandel.new_zoom = 2.0 
+					myMandel.DoZoomChange(true)
+					popup.Hide()
+				},),
+			),
+		)
+		popup = widget.NewModalPopUp(popUpContent,myWindow.Canvas())
+		popup.Show()
+	})
+	*/
 	menuItemQuit:= fyne.NewMenuItem("Quit", func() {
 		//fmt.Println("In DoQuit:")
 		os.Exit(0)
 	})
+//	menuControl:= fyne.NewMenu("Control", menuItemColor, menuItemZoom, menuItemQuit);
 	menuControl:= fyne.NewMenu("Control", menuItemColor, menuItemQuit);
 	// About Menu Set up
 	menuItemAbout := fyne.NewMenuItem("About...", func() {
@@ -462,7 +645,9 @@ func main() {
 	myWindow.SetMainMenu(mainMenu)
 
 	myRaster := canvas.NewRasterWithPixels(myMandel.DrawOneDot)
+//	myTappableRaster := NewTappableRaster(myRaster,DoRasterTap)
 //	myCanvas.SetContent(container.NewWithoutLayout(myRaster))
+//	myWindow.SetContent(myTappableRaster)
 	myWindow.SetContent(myRaster)
 //	myCanvas.Refresh(myRaster)
 //	myCanvas.Refresh(myRaster)
@@ -470,9 +655,14 @@ func main() {
 
 	go func() {
 		for { 
-			time.Sleep(time.Nanosecond * 10000)
+			time.Sleep(time.Nanosecond * 1000000)
 			if !myMandel.up_to_date {
 				myMandel.UpdateSome()
+			} else {
+				for delays:=0;delays<(101-myMandel.cur_roam_speed);delays++ {
+					time.Sleep(time.Nanosecond * 100000000)
+				}
+				myMandel.RoamAdjust()
 			}
 //			fmt.Println(doDraw,doQuit)
 		    myRaster.Refresh()
